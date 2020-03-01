ee1f210b7f783575e8e7053ed9163d672e652f98
Andrew Miller <andrew@amxl.com>
2020-03-01 22:11:20 +1100

    Improve documentation

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+/*
+    Sends a message to gotify server for each alert that fires from Prometheus
+    Alertmanager.
+    
+    Copyright (C) 2020  Andrew Miller
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+*/
 const http = require('http');
 const connect = require('connect');
 const bodyParser = require('body-parser');
